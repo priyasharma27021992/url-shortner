f77c4a36596a27e7ecc7a1028100ec54811e0e7f
Priya.Sharma <priyasharma2702@gmail.com>
2024-04-22 00:59:09 +0530

    added authoptions, create url, get ur;

--- src/app/api/urls/route.ts
+++ src/app/api/urls/route.ts
@@ -1,3 +1,46 @@
-import { NextRequest } from 'next/server';
+import { authOptions } from '@/src/lib/authOptions';
+import { createUrl, getUrlsForUser } from '@/src/lib/url';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {}
+export async function GET(req: NextRequest) {
+  // Get session data
+  const session = await getServerSession(authOptions);
+  if (!session || !session?.user) {
+    return NextResponse.redirect('/dashboard');
+  }
+
+  // get skip and take params from the URL
+  const {
+    nextUrl: { search },
+  } = req;
+  const urlSearchParams = new URLSearchParams(search);
+  const skip = parseInt(urlSearchParams.get('skip') ?? '0');
+  const take = Math.min(parseInt(urlSearchParams.get('take') ?? '10'), 100);
+  const data = await getUrlsForUser(session.user.id, skip, take);
+
+  return NextResponse.json({ data });
+}
+
+export async function POST(req: NextRequest) {
+  // Get Session Data
+  const session = await getServerSession(authOptions);
+  if (!session || !session?.user) {
+    return NextResponse.redirect('/dashboard');
+  }
+  try {
+    //   Get Original url from the body
+    const body = await req.json();
+    console.log('body', body);
+    const originalUrl = body.get('url');
+
+    //   Try to parse the url, will throw an exception if the url is not correct
+    new URL(originalUrl);
+
+    //   Create url object for the user
+    const url = await createUrl(originalUrl, session);
+    return NextResponse.json(url);
+  } catch (err) {
+    return NextResponse.json({ details: 'Invalid Url' }, { status: 400 });
+  }
+}
--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,4 +1,6 @@
+import { Session } from 'next-auth';
 import { prisma } from './prisma';
+import crypto from 'crypto';
 
 export const getUrlById = async (id: string) => {
   const url = await prisma.url.findUnique({
@@ -12,4 +14,62 @@ export const getUrlById = async (id: string) => {
   return url;
 };
 
-// export const
+export const createUrl = async (originalUrl: string, session: Session) => {
+  if (!session?.user?.email) {
+    throw new Error('No email present in session object');
+  }
+
+  let randomString = crypto.randomBytes(4).toString('hex');
+  let shortUrl = process.env.NEXTAUTH_URL + '/' + randomString;
+
+  while (true) {
+    const count = await prisma.url.count({ where: { shortUrl: shortUrl } });
+    if (count === 0) {
+      break;
+    }
+    randomString = crypto.randomBytes(4).toString('hex');
+    shortUrl = process.env.NEXTAUTH_URL + '/' + randomString;
+  }
+
+  const url = await prisma.url.create({
+    data: {
+      originalUrl,
+      shortUrl,
+      user: {
+        connect: {
+          email: session?.user?.email,
+        },
+      },
+    },
+  });
+  return url;
+};
+
+export const getUrlsForUser = async (
+  id: string,
+  skip: number = 0,
+  take: number = 10,
+): Promise<IPaginatedUrls> => {
+  const results = await Promise.all([
+    prisma.url.findMany({
+      skip: skip,
+      take: take,
+      where: { userId: id },
+      include: {
+        user: true,
+      },
+    }),
+    prisma.url.count({
+      where: {
+        userId: id,
+      },
+    }),
+  ]);
+
+  return {
+    results: results[0] ?? [],
+    totalCount: results[1] ?? 0,
+    skip: skip,
+    take: take,
+  };
+};
